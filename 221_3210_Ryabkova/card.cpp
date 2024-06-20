8355ac108a879b9d93cd885e56f0d215462f3e15
RyabkovaA <125076329+RyabkovaA@users.noreply.github.com>
2024-06-20 13:08:08 +0300

    displaying number on show button

--- 221_3210_Ryabkova/card.cpp
+++ 221_3210_Ryabkova/card.cpp
@@ -8,9 +8,9 @@ card::card(QWidget *parent)
     , m_number(0)
 {
     ui->setupUi(this);
-    int m_number = QRandomGenerator::global()->bounded(-1000, 1000);
-    ui->labelNumber->setText(QString::number(m_number));
-    // connect(ui->buttonShow, &QPushButton::clicked, this, &card::on_buttonShow_clicked);
+    // int m_number = QRandomGenerator::global()->bounded(-1000, 1000);
+    // ui->labelNumber->setText(QString::number(m_number));
+    connect(ui->buttonShow, &QPushButton::clicked, this, &card::on_buttonShow_clicked);
 
 }
 
